d4fd8e6571bd64df53f73122879bd812de54f831
christopher.mbaike <christopher.mbaike@thomsonreuters.com>
2015-12-02 16:58:15 +0000

    rearrange the structure of bean class

--- src/test/java/com/techboy/selenium/beanconfig/Beans.java
+++ src/test/java/com/techboy/selenium/beanconfig/Beans.java
@@ -5,7 +5,6 @@ import com.techboy.selenium.config.BrowserCapabilities;
 import org.openqa.selenium.Proxy;
 import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -17,7 +16,6 @@ import static org.openqa.selenium.Proxy.ProxyType.MANUAL;
  * Created by christopher on 01/12/2015.
  */
 @Configuration
-@Component
 public class Beans {
 
     String workingDir = System.getProperty("user.dir");
@@ -28,6 +26,18 @@ public class Beans {
     private final String proxyDetails = String.format("%s:%d", proxyHostname, proxyPort);
     private static String browser = System.getProperty("browser", "chrome");
 
+
+    @PostConstruct
+    public void systemPath() throws IOException {
+        if (workingOS.contains("windows")) {
+            System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath()+"/src/test/resources/selenium_browser_drivers/windowsChromedriver/chromedriver.exe");
+        } else if (workingOS.contains("mac")) {
+            System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath()+"/src/test/resources/selenium_browser_drivers/macChromedriver/chromedriver");
+        } else if (workingOS.contains("linux")) {
+            System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath()+"/src/test/resources/selenium_browser_drivers/linuxChromedriver/chromedriver");
+        }
+
+    }
 
     @Bean
     public Proxy proxy() {
@@ -54,17 +64,6 @@ public class Beans {
         return new BrowserDriverExtended.ChromeDriverExtended(BrowserCapabilities.newInstance().getChromeCapabilities());
     }
 
-    @PostConstruct
-    public void systemPath() throws IOException {
-        if (workingOS.contains("windows")) {
-              System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath()+"/src/test/resources/selenium_browser_drivers/windowsChromedriver/chromedriver.exe");
-        } else if (workingOS.contains("mac")) {
-             System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath()+"/src/test/resources/selenium_browser_drivers/macChromedriver/chromedriver");
-        } else if (workingOS.contains("linux")) {
-            System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath()+"/src/test/resources/selenium_browser_drivers/linuxChromedriver/chromedriver");
-        }
-
-    }
 
     public static class FirefoxCondition implements Condition {
         @Override
